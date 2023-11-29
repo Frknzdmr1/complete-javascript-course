478a53a90d0b73d5193b772ffadbc1c55be0177d
Furkan <ozdemirf97@icloud.com>
2023-11-29 07:02:27 -0500

    imp. new. btn

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //select Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //another way to select an ID
 const current0El = document.getElementById('current--0');
@@ -18,25 +20,61 @@ diceEl.classList.add('hidden');
 const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
-  //1.Generate random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-  //2. Display the Dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+  if (playing) {
+    //1.Generate random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //3. check for rolled 1
-  if (dice !== 1) {
-    //Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    current0El.textContent = currentScore; //satir 35 satir 36 biri dinamik biri hard code
-  } else {
-    //switch to next player
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    //2. Display the Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //3. check for rolled 1
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // current0El.textContent = currentScore; //satir 35 satir 36 biri dinamik biri hard code
+    } else {
+      //switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. Add current score to active player's score
+    score[activePlayer] += currentScore;
+    // scores[1] = score[1] + currentScore; bi ustteki olay bu
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+
+    //2.Chekc if player's score is >= 100
+    //Finish the game
+    if (score[activePlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--active');
+    } else {
+      switchPlayer();
+    }
   }
 });
